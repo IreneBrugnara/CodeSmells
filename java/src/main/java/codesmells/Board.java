d1db2fed414dd41fc279681289a6bae1ca954240
Dario Campagna <campagna@esteco.com>
2020-11-30 18:45:39 +0100

    Add constructor to Tile

--- java/src/main/java/codesmells/Board.java
+++ java/src/main/java/codesmells/Board.java
@@ -3,38 +3,28 @@ package codesmells;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Board
-{
+public class Board {
     private List<Tile> _plays = new ArrayList<>();
 
-    public Board()
-    {
-        for (int i = 0; i < 3; i++)
-        {
-            for (int j = 0; j < 3; j++)
-            {
-                Tile tile = new Tile();
-                tile.X = i;
-                tile.Y = j;
-                tile.Symbol = ' ';
-                _plays.add(tile);
+    public Board() {
+        for (int x = 0; x < 3; x++) {
+            for (int y = 0; y < 3; y++) {
+                _plays.add(new Tile(x, y, ' '));
             }
         }
     }
 
-    public Tile TileAt(int x, int y)
-    {
+    public Tile TileAt(int x, int y) {
         for (Tile t : _plays) {
-            if (t.X == x && t.Y == y){
+            if (t.X == x && t.Y == y) {
                 return t;
             }
         }
         return null;
     }
 
-    public void AddTileAt(char symbol, int x, int y)
-    {
-        TileAt(x,y).Symbol = symbol;
+    public void AddTileAt(char symbol, int x, int y) {
+        TileAt(x, y).Symbol = symbol;
     }
 
     char threeInARow() {
--- java/src/main/java/codesmells/Tile.java
+++ java/src/main/java/codesmells/Tile.java
@@ -5,4 +5,10 @@ public class Tile
     public int X;
     public int Y;
     public char Symbol;
+
+    public Tile(int x, int y, char symbol) {
+        X = x;
+        Y = y;
+        Symbol = symbol;
+    }
 }
